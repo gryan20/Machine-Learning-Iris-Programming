acd6875db7d2606e8f0fa8cf1287ecfceb6cde49
gryan20 <67970791+gryan20@users.noreply.github.com>
2020-08-15 10:23:02 -0500

    re-done
    went about it in a different way

--- src/NearestNeighbors.java
+++ src/NearestNeighbors.java
@@ -1,171 +1,98 @@
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.io.IOException;
 
 
 public class NearestNeighbors {
 
-	private List<Iris>testDataSet;
-	private List<Iris>trainingDataSet;
-	private List<Iris>condensedDataSet;
+	private static int    numberOfEntries = 75;
+	private static int    numberOfMeasurements = 4;
+	private static double trainingData[][];
+	private static String trainingClassifications[];
+	private static double testingData[][];
+	private static String testingClassifications[];
 	
 	public NearestNeighbors() {
 		
-		testDataSet = new ArrayList<Iris>();
-		trainingDataSet = new ArrayList<Iris>();
-		condensedDataSet = new ArrayList<Iris>();
+		trainingData = new double[numberOfEntries][numberOfMeasurements]; //contains all measurements for the training data
+		trainingClassifications = new String[numberOfEntries];			  //contains the classifications for the training data
+		testingData = new double[numberOfEntries][numberOfMeasurements];  //contains all measurements for the testing data
+		testingClassifications = new String[numberOfEntries];			  //contains the classifications for the testing data so we can check our accuracy
 	}
 	
 	public static void main(String[] args) throws IOException {
-		//Heading		
+		//Prints out heading
 		System.out.println("Programming Fundamentals");
 		System.out.println("Name: Gabby Ryan");
 		System.out.println("PROGRAMMING ASSIGNMENT 3");
 		System.out.println(" ");
-		DataReader read = new DataReader();
-		Scanner scan = new Scanner(System.in);
+		
+		Scanner scanner = new Scanner(System.in);
 		NearestNeighbors cnnImpl = new NearestNeighbors();
 
-	
-		System.out.println("Enter the path for the training data: ");
 		
-		String 	  irisPath = scan.nextLine();
-		// Store them as two different things
-		cnnImpl.SetIrisTrainingSet(read.getIrisData(irisPath));
-		System.out.println("Enter the path for the testing data: ");
-		irisPath = scan.nextLine();
-		System.out.println("EX#: TRUE LABEL, PREDICTED LABEL");
-		cnnImpl.SetIrisTestSet(read.getIrisData(irisPath));
-	
-		scan.close();
-
-		
-		cnnImpl.prepareCondensedSet();
-		
-		for (Iris i : cnnImpl.condensedDataSet)
-		{
-			System.out.println(i);
-		}
+		System.out.print("Enter the path for the training data: ");
+		String 	  irisPath = scanner.nextLine();
+		//getIrisData will parse the file and store data to the correct arrays
+		cnnImpl.getIrisData(irisPath, trainingData, trainingClassifications);
+		System.out.print("Enter the path for the testing data: ");
+		irisPath = scanner.nextLine();
+		cnnImpl.getIrisData(irisPath, testingData, testingClassifications);
+		scanner.close();
+		System.out.println(" ");
+		System.out.println("EX#:  TRUE LABEL, PREDICTED LABEL");
 		
 		double classifyAcc = cnnImpl.calculateClassificationAcc();
-		System.out.println("Classification accuracy: " + classifyAcc);
+		// print the classification accuracy
+		System.out.println("ACCURACY: " + classifyAcc);
 	}
 	
-	//Accuracy
-		private double calculateClassificationAcc()
+	private double calculateClassificationAcc()  
+	{
+		double minDist = 999999;
+		double correctClassification = 0;
+		int closestSample = 0;
+	
+		// for each test entry
+		for (int y = 0; y < numberOfEntries; y++)
 		{
-			double minDist = 999999;
-			double correctClassification = 0;
-			Iris closestSample = null;
+			minDist = 999999;
+			//calculate the closest training example and store the index in the closestSample variable
+			for (int x = 0; x < numberOfEntries; x++)
+			{
+				// index into the test and training arrays to get the measurements to calculate distance
+				double calcDistance = distance(trainingData[x][0], trainingData[x][1], trainingData[x][2], trainingData[x][3], 
+												testingData[y][0], testingData[y][1], testingData[y][2], testingData[y][3]);
+				// check if our new calculated distance is closer than our current closestSample
+				if(calcDistance < minDist)
+				{
+					minDist = calcDistance;
+					closestSample = x;
+				}
+			}
+			
+			// Check our testClassifications for entry y against our traininClassification of our closestSample
+			if (testingClassifications[y].equals(trainingClassifications[closestSample]))
+			{
+				// if they match we increment correctClassification
+				correctClassification++;
+			}
+
+			// print our entry number (index + 1) this creates the test label and predicted label
+			System.out.println( y + 1 + ": " + testingClassifications[y] + " " + trainingClassifications[closestSample]);
+		}
 		
-			for (Iris testD : testDataSet)
-			{
-				minDist = 999999;
-				for (Iris trainD : condensedDataSet)
-				{
-					double calcDistance = testD.distance(trainD);
-					if(calcDistance < minDist)
-					{
-						minDist = calcDistance;
-						closestSample = trainD;
-					}
-				}
-				if (testD.type == closestSample.type)
-				{
-					correctClassification++;
-				}
-			}
-			
-			double classAccuracy  = correctClassification/(testDataSet.size());
-			return classAccuracy;
-		}
-		
-			
-		public void prepareCondensedSet()
-		{
-			condensedDataSet.add(trainingDataSet.get(0));
-			
-			int unchangedCount = 0;
-			List<Iris>reducedSet = constructReducedSet();
-			
-			int previousReducedSetSize = 0;
-			while (true)
-			{
-				previousReducedSetSize = reducedSet.size();
-				int i = 0;
-				while (!reducedSet.isEmpty())
-				{
-					Iris ir = reducedSet.get(0);
-					double minDist = 999999;
-					IrisType type =IrisType.SETOSA;
-					Iris ic = null;
-				
-					for (int j = 0; j < condensedDataSet.size(); j++)
-					{
-						ic = condensedDataSet.get(j);
-						double distBetweenPattern = ir.distance(ic);
-						if (distBetweenPattern < minDist)
-						{
-							minDist = distBetweenPattern;
-							type = ic.type;
-						}
-					}
-
-					if (ir.type != type)
-					{
-						condensedDataSet.add(ir);
-					}
-
-					reducedSet.remove(ir);
-				}
-
-				reducedSet = constructReducedSet();
-				if (previousReducedSetSize == reducedSet.size())
-				{
-					unchangedCount++;
-				}
-				else
-				{
-					unchangedCount = 0;
-				}
-				if (unchangedCount == 2) {
-					break;
-				}
-			}
-		}
-		private List<Iris>constructReducedSet()
-		{
-			List<Iris> reducedSet = new ArrayList<Iris>();
-			for (Iris iTrain : trainingDataSet)
-			{
-				if (condensedDataSet.contains(iTrain))
-				{
-					continue;
-				}
-				reducedSet.add(iTrain);
-			}
-
-			return reducedSet;
-		}
-			
-		public void SetIrisTrainingSet(List<Iris>irisDataSet)
-		{
-				this.trainingDataSet = irisDataSet;
-		}
-			
-		public void SetIrisTestSet(List<Iris>irisDataSet)
-		{
-				this.testDataSet = irisDataSet;
-		}
-}
-class DataReader{
-	public List<Iris> getIrisData(String irisFilePath) throws IOException
+		// calculates accuracy
+		double classAccuracy  = correctClassification/numberOfEntries;
+		return classAccuracy;
+	}
+	
+	public void getIrisData(String irisFilePath, double data[][], String classification[]) throws IOException
 	{
 		Scanner irisDataReader = new Scanner(new FileReader(irisFilePath));
-		List<Iris> irisDataSet= new ArrayList<Iris>();
+		int i = 0;
 
+		// for all the data in the input file
 		while(irisDataReader.hasNextLine())
 		{
 			String input = irisDataReader.nextLine();
@@ -178,87 +105,24 @@ class DataReader{
 					
 			if (attr != null && attr.length == 5)
 			{
-				IrisType type = IrisType.getType(attr[4]);
-				Iris sample = new Iris(Double.parseDouble(attr[0]),
-						Double.parseDouble(attr[1]),
-						Double.parseDouble(attr[2]),
-						Double.parseDouble(attr[3]),
-						type);
-				irisDataSet.add(sample);
+				// parse all measurements as doubles into our data array
+				data[i][0] = Double.parseDouble(attr[0]);
+				data[i][1] = Double.parseDouble(attr[1]);
+				data[i][2] = Double.parseDouble(attr[2]);
+				data[i][3] = Double.parseDouble(attr[3]);
+				// add the classification to the classification array
+				classification[i] = attr[4];
+				i++;
 			}
 		}
-			return irisDataSet;
-	}
-}
-
-class Iris
-{
-	double sepalLength;
-	double sepalWidth;
-	double petalLength;
-	double petalWidth;
-	IrisType type;
-
-	Iris(double sl, double sw, double pl, double pw, IrisType t)
-	{
-		sepalLength = sl;
-		sepalWidth = sw;
-		petalLength = pl;
-		petalWidth = pw;
-		type = t;
-	}
-			
-	public String toString()
-	{
-		return type.getLabel() + type.getLabel();
-	}
-			
-	public boolean equals(Iris other)
-	{
-		return ((this.type == other.type));
 	}
 	
-	//used for accuracy
-	public double distance(Iris other)
+	public double distance(double slx, double swx, double plx, double pwx, double sly, double swy, double ply, double pwy)
 	{
-		double d1 = Math.pow((this.sepalLength - other.sepalLength), 2);
-		double d2 = Math.pow((this.sepalWidth - other.sepalWidth), 2);
-		double d3 = Math.pow((this.petalWidth - other.petalWidth), 2);
-		double d4 = Math.pow((this.petalLength - other.petalLength),2);
+		double d1 = Math.pow((slx - sly), 2);
+		double d2 = Math.pow((swx - swy), 2);
+		double d3 = Math.pow((plx - ply), 2);
+		double d4 = Math.pow((pwx - pwy),2);
 		return Math.sqrt(d1 + d2 + d3 + d4);
-	}
-			
-}
-enum IrisType
-{
-	SETOSA(1, "Iris-setosa"), VERSICOLOR(2,"Versicolor"), VIRGINICA(3,"Virginica");
-	private int code;
-	private String label;
-
-	IrisType(int code, String label)
-	{
-		this.code = code;
-		this.label = label;
-	}
-	public int getCode()
-	{
-		return this.code;
-	}
-	public String getLabel()
-	{
-		return this.label;
-	}
-	public static IrisType getType(String type)
-	{
-		if ("Iris-versicolor".equals(type))
-		{
-			return VERSICOLOR;
-		}
-		if ("Iris-virginica".equals(type))
-		{
-			return VIRGINICA;
-		}
-		return SETOSA;
-	
 	}
 }
